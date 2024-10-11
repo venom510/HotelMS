8a1c67131db598a4989837adfb2c36a848f8059a
Manav Upadhyay <manavupadhyay5@gmail.com>
2024-10-12 01:55:14 +0530

    Updated root package name

--- hotel-ms/src/main/java/com/hoteladvisor/hotel/HotelMsApplication.java
+++ hotel-ms/src/main/java/com/hoteladvisor/hotel/HotelMsApplication.java
@@ -1,4 +1,4 @@
-package hotel_ms;
+package com.hoteladvisor.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hotel-ms/src/test/java/com/hoteladvisor/hotel/HotelMsApplicationTests.java
+++ hotel-ms/src/test/java/com/hoteladvisor/hotel/HotelMsApplicationTests.java
@@ -1,4 +1,4 @@
-package hotel_ms;
+package com.hoteladvisor.hotel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
